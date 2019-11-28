8038705cfb62b561fd1305d3039d677a2e5dd559
srz2 <srz222@gmail.com>
2019-11-28 02:52:02 -0500

    Added IP Address to config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
+    IP_ADDRESS: process.env.IP_ADDRESS,
     PORT: process.env.PORT,
     MONGO_USER: process.env.MONGO_USER,
     MONGO_PASSWORD: process.env.MONGO_PASSWORD,
